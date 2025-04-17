d0f1c2b890082b09629406bde5cec8f26e574a0f
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-04-17 18:20:20 +0530

    fix: display only first 15 characters in sidebar conversation title

--- src/components/sidebar/components/CategoryList.jsx
+++ src/components/sidebar/components/CategoryList.jsx
@@ -8,21 +8,26 @@ const CategoryList = ({ title, conversations }) => {
     return (
         <div className="mb-7">
             <p className="text-sm mb-4 text-stone-400">{title}</p>
-            {conversations.map((value, index) => (
-                <NavLink
-                    to={value.id}
-                    key={value.id}
-                    className={({ isActive }) => isActive ? "[&>button]:bg-stone-800" : ""}
-                >
-                    <button
+            {conversations.map((value, index) => {
+                const title = value.title
+                const trimmedTitle = title.substring(0, 17)
+
+                return (
+                    <NavLink
+                        to={value.id}
                         key={value.id}
-                        className="my-2 pl-2 gap-3 flex p-1 hover:bg-stone-800 w-full text-start rounded text-sm text-stone-300 items-center"
+                        className={({ isActive }) => isActive ? "[&>button]:bg-stone-800" : ""}
                     >
-                        <MessageSquareText size={15} />
-                        {value.title}
-                    </button>
-                </NavLink>
-            ))}
+                        <button
+                            key={value.id}
+                            className="my-2 pl-2 gap-3 flex p-1 hover:bg-stone-800 w-full text-start rounded text-sm text-stone-300 items-center"
+                        >
+                            <MessageSquareText size={15} />
+                            {trimmedTitle} {trimmedTitle === title ? "" : "..."}
+                        </button>
+                    </NavLink>
+                )
+            })}
         </div>
     );
 };
