d20f710bec56bd2805fc3f0d2e0b60320ee196ac
shivanandan-dev <shivanandan.dhanasekara@solitontech.com>
2025-04-18 13:01:01 +0530

    fix: modify sidebar width

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { Plus } from "lucide-react"
-import { useEffect } from "react"
 import { Link } from "react-router"
 import { useConversation } from "../../context/ConversationContext"
 import ConversationList from "./components/ConversationList"
@@ -10,7 +9,7 @@ export default function Sidebar() {
   const { conversations } = useConversation()
 
   return (
-    <div className="bg-stone-900 w-[15%] h-screen overflow-hidden flex justify-center">
+    <div className="bg-stone-900 w-[300px] h-screen overflow-hidden flex justify-center">
       <div className="w-[90%]">
         <Link to="/">
           <button className="border w-full py-2 px-2 rounded-md flex gap-2 mt-4 hover:border-slate-300 hover:text-slate-300">
